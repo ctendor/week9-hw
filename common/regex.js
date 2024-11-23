c1f272e6f8b356c5909a9b40580fc2e7ea6ecb1c
Ubuntu <ubuntu@ip-172-31-6-254.ap-northeast-2.compute.internal>
2024-11-23 12:25:18 +0000

    regex function 생성

--- common/regex.js
+++ common/regex.js
@@ -1,3 +1,44 @@
-var idRegex = /([a-zA-Z0-9].{8,20})/;
-var pwRegex = /(?=.*\d)(?=.*[a-zA-Z]).{8,20}/;
-var emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+export function validateId(id) {
+  const idRegex = /^[a-zA-Z0-9]{8,20}$/;
+  if (idRegex.test(id)) {
+    return {
+      isValid: true,
+      message: "아이디가 유효합니다.",
+    };
+  } else {
+    return {
+      isValid: false,
+      message: "아이디는 8~20자 영문/숫자로 입력하세요.",
+    };
+  }
+}
+
+export function validatePassword(password) {
+  const pwRegex = /(?=.*\d)(?=.*[a-zA-Z]).{8,20}/;
+  if (pwRegex.test(password)) {
+    return {
+      isValid: true,
+      message: "비밀번호가 유효합니다.",
+    };
+  } else {
+    return {
+      isValid: false,
+      message: "비밀번호는 8~20자, 영문과 숫자를 포함해야 합니다.",
+    };
+  }
+}
+
+export function validateEmail(email) {
+  const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+  if (emailRegex.test(email)) {
+    return {
+      isValid: true,
+      message: "이메일이 유효합니다.",
+    };
+  } else {
+    return {
+      isValid: false,
+      message: "올바른 이메일 주소를 입력하세요.",
+    };
+  }
+}
